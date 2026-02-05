7b01c96d6a8573a0d1f577254a8684ca755cc0f5
DominikHil <Dominik.Hildebrand@student.uni-tuebingen.de>
2026-02-05 12:31:17 +0100

    Slightly adjusted test.py for evaluation submission

--- test.py
+++ test.py
@@ -98,7 +98,7 @@ def compute_metrics(outputs, labels, queries_mask, labels_mask, results_dict, io
 def print_metrics(metrics):
     metrics["precision"] = metrics["tp"] / (metrics["tp"]+ metrics["fp"]) if (metrics["tp"]+ metrics["fp"]) > 0 else 0
     metrics["recall"] = metrics["tp"] / (metrics["tp"]+ metrics["fn"]) if (metrics["tp"]+ metrics["fn"]) > 0 else 0
-    metrics["f1"] = 2 * metrics["Precision"] * metrics["Recall"] / (metrics["Recall"]+ metrics["Precision"]) if (metrics["Recall"]+ metrics["Precision"]) > 0 else 0
+    metrics["f1"] = 2 * metrics["precision"] * metrics["recall"] / (metrics["precision"] + metrics["recall"]) if (metrics["precision"]+ metrics["recall"]) > 0 else 0
     metrics["mean-IoU"] = metrics["IoU"] / metrics["tp_match"] if metrics["tp_match"] > 0 else 0
     for k,v in metrics.items():
         print(f"{k}:".ljust(6), v)
